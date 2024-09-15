9665686cd685ea49a0c812b4f9abfa34ae7beef8
savannah-thomas <savannah.thomas@lusfiber.net>
2024-09-14 22:23:30 -0500

    Class that calculates average of two numbers provided by user input in main.

--- Average.java
+++ Average.java
@@ -1,6 +1,8 @@
 public class Average {
-    public double calculateAverage (int number1, int number2){
+    public double calculateAverage(int number1, int number2) {
         return (number1 + number2) / 2.0;
     }
-
 }
+//class used to calculate average of two numbers provided by user in main
+
+
